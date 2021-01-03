c6f89a78c792ff10ce26a7339cb7a3dd64ef4b45
gouflv <lv.gouf@gmail.com>
2021-01-04 01:49:10 +0800

    test(lang/typeOf): add case

--- src/javascript/lang/typeof/typeOf.test.js
+++ src/javascript/lang/typeof/typeOf.test.js
@@ -4,6 +4,7 @@ describe('Test typeOf', () => {
   it('get typeOf primitive values', () => {
     expect(typeOf(undefined)).toBe('undefined')
     expect(typeOf(null)).toBe('object')
+    expect(typeOf(NaN)).toBe('number')
     expect(typeOf(true)).toBe('boolean')
     expect(typeOf(1)).toBe('number')
     expect(typeOf('foo')).toBe('string')
@@ -12,12 +13,15 @@ describe('Test typeOf', () => {
   it('get typeOf build-ins objects', () => {
     expect(typeOf([])).toBe('array')
     expect(typeOf({})).toBe('object')
+    expect(typeOf(() => {})).toBe('function')
     expect(typeOf(new Set())).toBe('set')
   })
 
   it('get typeOf class instance', () => {
     function Foo() {}
-    const o = new Foo()
-    expect(typeOf(o)).toBe('foo')
+    expect(typeOf(new Foo())).toBe('foo')
+
+    class Bar {}
+    expect(typeOf(new Bar())).toBe('bar')
   })
 })
