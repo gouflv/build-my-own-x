0047b8983535d62b9f4d6f124b8b75b331576999
gouflv <lv.gouf@gmail.com>
2021-01-09 00:17:17 +0800

    feature(object): implement keys

--- src/javascript/object/keys/keys.js
+++ src/javascript/object/keys/keys.js
@@ -1,1 +1,14 @@
-export const keys = val => Object.keys(val)
+import { isObject } from '../../lang/is/is'
+
+export const keys =
+  Object.keys ||
+  (obj => {
+    if (!isObject(obj)) return []
+    const keys = []
+    for (const k in obj) {
+      if (Object.hasOwnProperty.call(obj, k)) {
+        keys.push(k)
+      }
+    }
+    return keys
+  })
