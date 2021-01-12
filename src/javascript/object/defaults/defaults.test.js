729b49e17b86738dfe7ac070ca3344a30e9abb1c
gouflv <lv.gouf@gmail.com>
2021-01-12 21:19:14 +0800

    test(object/default): add origin value test case

--- src/javascript/object/defaults/defaults.test.js
+++ src/javascript/object/defaults/defaults.test.js
@@ -2,6 +2,18 @@ import { defaults } from './defaults'
 
 describe('Test defaults', () => {
   it('simple defaults', () => {
+    expect(defaults()).toStrictEqual({})
+    expect(defaults(undefined)).toStrictEqual({})
+    expect(defaults(null)).toStrictEqual({})
+    expect(defaults(1)).toStrictEqual(new Number(1))
+    expect(defaults(true)).toStrictEqual(new Boolean(true))
+    expect(defaults('')).toStrictEqual(new String(''))
+    expect(defaults([])).toStrictEqual([])
+    expect(defaults({}, [])).toStrictEqual({})
+    expect(defaults([1], [1, 2])).toStrictEqual([1, 2])
+  })
+
+  it('object defaults', () => {
     const a = { a: 1 }
     const b = { b: 2 }
     const c = { a: 3 }
