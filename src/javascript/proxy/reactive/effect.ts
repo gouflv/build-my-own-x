b947e79f8e87f5da01d989a89aafd59269e0e964
gouflv <lv.gouf@gmail.com>
2021-01-16 16:50:58 +0800

    refactor(proxy/reactive): init effect track

--- src/javascript/proxy/reactive/effect.ts
+++ src/javascript/proxy/reactive/effect.ts
@@ -1,5 +1,30 @@
+import { activeEffect } from './effectRunner'
+
+export type Target = object
+export interface Effect {}
+
 export const enum TrackOpType {
   GET = 'get'
 }
 
-export const track = (target, key, type: TrackOpType) => {}
+const trackTargetMap = new WeakMap<Target>()
+
+export const track = (target: Target, key, type: TrackOpType) => {
+  if (!activeEffect) {
+    return
+  }
+
+  let trackedTarget = trackTargetMap.get(target)
+  if (!trackedTarget) {
+    trackedTarget.set(target, (trackedTarget = new Map()))
+  }
+
+  let trackedTargetKeySet = trackedTarget.get(key)
+  if (!trackedTargetKeySet) {
+    trackedTarget.set(key, (trackedTargetKeySet = new Set()))
+  }
+
+  if (!trackedTargetKeySet.has(activeEffect)) {
+    trackedTargetKeySet.add(activeEffect)
+  }
+}
---
+++ src/javascript/proxy/reactive/effectRunner.ts
@@ -0,0 +1,3 @@
+import { Effect } from './effect'
+
+export let activeEffect: Effect | undefined
