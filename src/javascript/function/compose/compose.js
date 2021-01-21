18912252d608b2e23754530048c3ecb8094aebf1
gouflv <lv.gouf@gmail.com>
2021-01-22 01:13:58 +0800

    refactor(function): implement compose with reduceRight

--- src/javascript/function/compose/compose.js
+++ src/javascript/function/compose/compose.js
@@ -1,4 +1,6 @@
-export const compose = (...f) => {
+import { reduceRight } from '../../array/reduceRight/reduceRight'
+
+const composeOld = (...f) => {
   return (...args) => {
     let index = f.length - 1
     let result = f[index].apply(this, args)
@@ -6,5 +8,15 @@ export const compose = (...f) => {
       result = f[index].call(this, result)
     }
     return result
+  }
+}
+
+export const compose = (...fn) => {
+  return (...args) => {
+    return reduceRight(
+      fn.slice(0, -1),
+      (res, f) => f.call(this, res),
+      fn[fn.length - 1].apply(this, args)
+    )
   }
 }
