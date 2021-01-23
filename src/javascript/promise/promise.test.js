ed263672fa1e49dc451d581faf4c5f623808de71
gouflv <lv.gouf@gmail.com>
2021-01-23 21:10:51 +0800

    test(promise/promiseMock)

--- src/javascript/promise/promise.test.js
+++ src/javascript/promise/promise.test.js
@@ -1,8 +1,14 @@
 import { PromiseMock } from './promise'
+import { call } from '../function/apply/call'
+
+const expectFnCalledWith = (fn, args) =>
+  args.forEach((v, i) => expect(fn).nthCalledWith(i + 1, v))
 
 describe('Test PromiseMock', () => {
   it('promise simple', done => {
     const called = jest.fn()
+
+    called('start')
 
     new PromiseMock(resolve => {
       called('init')
@@ -14,10 +20,42 @@ describe('Test PromiseMock', () => {
       .then(() => {
         called('thenB')
       })
+      .then()
       .then(() => {
-        expect(called).nthCalledWith(1, 'init')
-        expect(called).nthCalledWith(2, 'thenA')
-        expect(called).nthCalledWith(3, 'thenB')
+        expectFnCalledWith(called, ['start', 'init', 'end', 'thenA', 'thenB'])
+        done()
+      })
+
+    called('end')
+  })
+
+  it('should resolve with value', done => {
+    const called = jest.fn()
+
+    new PromiseMock(resolve => {
+      resolve('resolved value')
+    })
+      .then(value => {
+        called(value)
+        return 'then value'
+      })
+      .then(() => {
+        expectFnCalledWith(called, ['resolved value'])
+        done()
+      })
+  })
+
+  it('should reject with reason', done => {
+    const called = jest.fn()
+
+    new PromiseMock((resolve, reject) => {
+      reject('reject reason')
+    })
+      .then(null, reason => {
+        called(reason)
+      })
+      .then(null, () => {
+        expectFnCalledWith(called, ['reject reason'])
         done()
       })
   })
