24814c16d3e2f421cc405718df99c7648becd0d4
gouflv <lv.gouf@gmail.com>
2021-02-05 19:02:47 +0800

    feature(data): hash-table

---
+++ src/data-structure/hash-table/hash-table.ts
@@ -0,0 +1,17 @@
+export class HashTable {
+  constructor() {}
+
+  set(key: string, value) {}
+
+  get(key: string) {}
+
+  has(key: string) {}
+
+  remove(key: string) {}
+
+  keys() {}
+
+  values() {}
+
+  protected hash(key: string) {}
+}
