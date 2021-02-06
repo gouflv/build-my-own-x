65b2e973faa3e52fc999e1dfb10f6634b01ec49b
gouflv <lv.gouf@gmail.com>
2021-02-06 21:03:35 +0800

    feature(alg): insertion sort done

--- src/algorithms/sort/insertion.ts
+++ src/algorithms/sort/insertion.ts
@@ -7,16 +7,13 @@ export const insertionSort = (arr: number[]) => {
   for (let i = 1; i < arr.length; i++) {
     curr = arr[i]
 
-    j = i - 1
-    while (j >= 0 && arr[j] > curr) {
-      j--
+    // move j to j + 1
+    for (j = i - 1; j >= 0 && arr[j] > curr; j--) {
+      arr[j + 1] = arr[j]
     }
-
-    console.log(`move ${j}:${arr[j]} to ${i}:${curr}`)
-
-    // ;[arr[i], arr[j + 1]] = [arr[j + 1], arr[i]]
+    // set curr to j + 1
+    arr[j + 1] = curr
   }
 
-  console.log(arr)
   return arr
 }
