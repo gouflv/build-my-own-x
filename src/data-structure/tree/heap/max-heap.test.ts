5d0e993dcfd09c6becb34d57a67e66a672287f34
gouflv <lv.gouf@gmail.com>
2021-02-22 19:15:17 +0800

    test(data): init heap

--- src/data-structure/tree/heap/max-heap.test.ts
+++ src/data-structure/tree/heap/max-heap.test.ts
@@ -8,13 +8,22 @@ describe('Test MaxHeap', () => {
 
   it('should add item and heapify it up', () => {
     maxHeap.add(5)
+    expect(maxHeap.peek()).toBe(5)
+
     maxHeap.add(3)
+    expect(maxHeap.peek()).toBe(5)
     expect(maxHeap.toString()).toBe('5,3')
+
     maxHeap.add(10)
+    expect(maxHeap.peek()).toBe(10)
     expect(maxHeap.toString()).toBe('10,3,5')
+
     maxHeap.add(1)
+    expect(maxHeap.peek()).toBe(10)
     expect(maxHeap.toString()).toBe('10,3,5,1')
+
     maxHeap.add(1)
+    expect(maxHeap.peek()).toBe(10)
     expect(maxHeap.toString()).toBe('10,3,5,1,1')
   })
 })
--- src/data-structure/tree/heap/min-heap.test.ts
+++ src/data-structure/tree/heap/min-heap.test.ts
@@ -8,13 +8,22 @@ describe('Test MinHeap', () => {
 
   it('should add item and heapify it up', () => {
     minHeap.add(5)
+    expect(minHeap.peek()).toBe(5)
+
     minHeap.add(3)
+    expect(minHeap.peek()).toBe(3)
     expect(minHeap.toString()).toBe('3,5')
+
     minHeap.add(10)
+    expect(minHeap.peek()).toBe(3)
     expect(minHeap.toString()).toBe('3,5,10')
+
     minHeap.add(1)
+    expect(minHeap.peek()).toBe(1)
     expect(minHeap.toString()).toBe('1,3,10,5')
+
     minHeap.add(1)
+    expect(minHeap.peek()).toBe(1)
     expect(minHeap.toString()).toBe('1,1,10,5,3')
   })
 })
