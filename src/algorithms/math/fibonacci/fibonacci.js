4450d24d90e158fbf36142d35c50427ced9e7c55
gouflv <lv.gouf@gmail.com>
2022-03-24 17:11:40 +0800

    feature(math): fibonacci sequence

--- src/algorithms/math/fibonacci/fibonacci.js
+++ src/algorithms/math/fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-export const fibonacci = n => {
+/**
+ * f(n) = f(n-1) + f(n-2)
+ */
+export const fibonacciN = n => {
   if (n === 0) return 0
   if (n === 1) return 1
-  return fibonacci(n - 2) + fibonacci(n - 1)
+  return fibonacciN(n - 2) + fibonacciN(n - 1)
+}
+
+export const fibonacciSequence = len => {
+  const res = []
+  let p1 = 0,
+    p2 = 1,
+    _n
+
+  for (let i = 0; i < len; i++) {
+    res.push(p1)
+    _n = p1 + p2
+    p1 = p2
+    p2 = _n
+  }
+  return res
 }
--- src/algorithms/math/fibonacci/fibonacci.test.js
+++ src/algorithms/math/fibonacci/fibonacci.test.js
@@ -1,10 +1,22 @@
-import { fibonacci } from './fibonacci'
+import { fibonacciN, fibonacciSequence } from './fibonacci'
+
+const s = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
 
 describe('Test fibonacci', () => {
-  it('simple', () => {
-    const s = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144]
+  it('Get the value in fibonacci sequence at index', () => {
     s.forEach((v, i) => {
-      expect(fibonacci(i)).toBe(v)
+      expect(fibonacciN(i)).toBe(v)
+    })
+  })
+
+  it('Get fibonacci sequence by length', () => {
+    expect(fibonacciSequence(0)).toEqual([])
+    expect(fibonacciSequence(1)).toEqual([0])
+    expect(fibonacciSequence(2)).toEqual([0, 1])
+    expect(fibonacciSequence(3)).toEqual([0, 1, 1])
+    expect(fibonacciSequence(4)).toEqual([0, 1, 1, 2])
+    s.forEach((v, i) => {
+      expect(fibonacciSequence(i + 1)).toEqual(s.slice(0, i + 1))
     })
   })
 })
